5a00151fbc69725d2d8f336fab026ba34e917c42
Sco1037 <sco1037@wgu.edu>
2025-12-04 16:31:32 -0500

    Implemented tests in data_check

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -84,6 +84,11 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert np.isfinite(kl_div) and kl_div < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+# checks if dataset size is reasonable (not too small, not too large)
+def test_row_count(data):
+        assert 15000 < data.shape[0] < 1000000
+
+# checks price range is between min_price and max_price
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all(), \
+        f"Some prices are outside the range {min_price} - {max_price}"
